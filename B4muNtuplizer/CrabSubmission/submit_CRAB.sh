d056692ac753325fabceb4ebb7db57b1983fe980
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-11-27 16:29:16 +0100

    Update submit_CRAB.sh

--- B4muNtuplizer/CrabSubmission/submit_CRAB.sh
+++ B4muNtuplizer/CrabSubmission/submit_CRAB.sh
@@ -9,26 +9,45 @@ fi
 # Assegna i nomi dei file ai singoli argomenti
 era=$1
 year=$2
+directory="$PWD"
+home_dir=$(dirname "$(dirname "$directory")/CrabSubmission")
+
+echo "La parte del percorso prima di 'pippo' è: $parte_prima_di_pippo"
+
+2022_C=("Run2022C-PromptReco-v1" "Run2022C-PromptReco-v1" "Run2022C-PromptReco-v1" "Run2022C-PromptReco-v1" "Run2022C-PromptReco-v1" "Run2022C-PromptReco-v1" "Run2022C-PromptReco-v1" "Run2022C-PromptReco-v1")
 
 if [ "${year}" == "2022" ]; then
     case "$era" in
-      C|D-v1|D-v2)
-        echo "Hai scelto B."
-        # Fai qualcosa per la scelta B
+      C)
+        echo "Era C."
+        Data_ID=("${2022_C[@]}")
+        path=""
         ;;
-      E)
+      D-v1)
         echo "Hai scelto C."
         # Fai qualcosa per la scelta C
         ;;
-      F|G)
+      D-v2)
         echo "Hai scelto K."
         # Fai qualcosa per la scelta K
         ;;
-      MC_pre)
+      E)
         echo "Hai scelto K."
         # Fai qualcosa per la scelta K
         ;;
-      MC_post)
+      F)
+        echo "Hai scelto K."
+        # Fai qualcosa per la scelta K
+        ;;
+      G)
+        echo "Hai scelto K."
+        # Fai qualcosa per la scelta K
+        ;;
+      MC_pre*)
+        echo "Hai scelto K."
+        # Fai qualcosa per la scelta K
+        ;;
+      MC_post*)
         echo "Hai scelto K."
         # Fai qualcosa per la scelta K
         ;;
@@ -39,9 +58,26 @@ if [ "${year}" == "2022" ]; then
     esac
 fi
 
+if [[ "$era" != *"MC_"* ]]; then
+    mkdir -p "${year}_era${era}"
+    for i in {0..7}; do
+        echo "Stream $i"
+        cp templates/CRAB_template.py "${year}_era${era}/CRAD_stream_${i}.py"
+        sed -i "s#YEAR#${year}#g" ${year}_era${era}/CRAD_stream_${i}.py
+        sed -i "s#ERANAME#${era}#g" ${year}_era${era}/CRAD_stream_${i}.py
+        sed -i "s#NUMBER#${i}#g" ${year}_era${era}/CRAD_stream_${i}.py
+        sed -i "s#DATASET_ID#${Data_ID[${i}]}#g" ${year}_era${era}/CRAD_stream_${i}.py
+        sed -i "s#FILE_TO_SUBMIT_PATH#${path}#g" ${year}_era${era}/CRAD_stream_${i}.py
+        
 
-# Modifica i file con sed
-sed -i 's/vecchia_stringa/nuova_stringa/g' "$file1" "$file2" "$file3" "$file4" "$file5"
+        
+        
+    done
+else
+    mkdir "${year}_MC"
+    echo "\$era contiene 'MC_'."
+fi
 
-echo "Modifica completata con successo nei file: $file1 $file2 $file3 $file4 $file5."
+
+
 
