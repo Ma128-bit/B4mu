517e55c8c835545dc0db4d2c8f35638fe96d7fdc
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-11-27 16:52:18 +0100

    Update submit_CRAB.sh

--- B4muNtuplizer/CrabSubmission/submit_CRAB.sh
+++ B4muNtuplizer/CrabSubmission/submit_CRAB.sh
@@ -14,6 +14,13 @@ home_dir=$(dirname "$(dirname "$directory")/CrabSubmission")
 echo "Home dir: $home_dir"
 
 declare -a C_2022=("Run2022C-PromptReco-v1" "Run2022C-PromptReco-v1" "Run2022C-PromptReco-v1" "Run2022C-PromptReco-v1" "Run2022C-PromptReco-v1" "Run2022C-PromptReco-v1" "Run2022C-PromptReco-v1" "Run2022C-PromptReco-v1")
+declare -a D-v1_2022=("Run2022D-PromptReco-v1" "Run2022D-PromptReco-v1" "Run2022D-PromptReco-v1" "Run2022D-PromptReco-v1" "Run2022D-PromptReco-v1" "Run2022D-PromptReco-v1" "Run2022D-PromptReco-v1" "Run2022D-PromptReco-v1")
+declare -a D-v2_2022=("Run2022D-PromptReco-v2" "Run2022D-PromptReco-v2" "Run2022D-PromptReco-v2" "Run2022D-PromptReco-v2" "Run2022D-PromptReco-v2" "Run2022D-PromptReco-v2" "Run2022D-PromptReco-v2" "Run2022D-PromptReco-v2")
+declare -a E_2022=("Run2022E-PromptReco-v1" "Run2022E-PromptReco-v1" "Run2022E-PromptReco-v1" "Run2022E-PromptReco-v1" "Run2022E-PromptReco-v1" "Run2022E-PromptReco-v1" "Run2022E-PromptReco-v1" "Run2022E-PromptReco-v1")
+declare -a F_2022=("Run2022F-22Sep2023-v1" "Run2022F-22Sep2023-v1" "Run2022F-22Sep2023-v1" "Run2022F-22Sep2023-v1" "Run2022F-22Sep2023-v1" "Run2022F-22Sep2023-v1" "Run2022F-22Sep2023-v1" "Run2022F-22Sep2023-v1")
+declare -a G_2022=("Run2022G-22Sep2023-v1" "Run2022G-22Sep2023-v1" "Run2022G-22Sep2023-v1" "Run2022G-22Sep2023-v1" "Run2022G-22Sep2023-v2" "Run2022G-22Sep2023-v1" "Run2022G-22Sep2023-v1" "Run2022G-22Sep2023-v1")
+declare -a Pre_E_MC=()
+declare -a Post_E_MC=()
 
 if [ "${year}" == "2022" ]; then
     case "$era" in
@@ -24,32 +31,44 @@ if [ "${year}" == "2022" ]; then
         golden_json=
         ;;
       D-v1)
-        echo "Hai scelto C."
-        # Fai qualcosa per la scelta C
+        echo "Era D-v1."
+        Data_ID=("${D-v1_2022[@]}")
+        path=""
+        golden_json=
         ;;
       D-v2)
-        echo "Hai scelto K."
-        # Fai qualcosa per la scelta K
+        echo "Era D-v2."
+        Data_ID=("${D-v2_2022[@]}")
+        path=""
+        golden_json=
         ;;
       E)
-        echo "Hai scelto K."
-        # Fai qualcosa per la scelta K
+        echo "Era E."
+        Data_ID=("${E_2022[@]}")
+        path=""
+        golden_json=
         ;;
       F)
-        echo "Hai scelto K."
-        # Fai qualcosa per la scelta K
+        echo "Era F."
+        Data_ID=("${F_2022[@]}")
+        path=""
+        golden_json=
         ;;
       G)
-        echo "Hai scelto K."
-        # Fai qualcosa per la scelta K
+        echo "Era G."
+        Data_ID=("${G_2022[@]}")
+        path=""
+        golden_json=
         ;;
-      MC_pre*)
-        echo "Hai scelto K."
-        # Fai qualcosa per la scelta K
+      MC_pre)
+        echo "MC ${era}."
+        path=""
+        datasets=("${G_2022[@]}")
         ;;
-      MC_post*)
-        echo "Hai scelto K."
-        # Fai qualcosa per la scelta K
+      MC_post)
+        echo "MC ${era}."
+        path=""
+        datasets=("${G_2022[@]}")
         ;;
       *)
         echo "Errore: era non corretta."
@@ -62,7 +81,7 @@ if [[ "$era" != *"MC_"* ]]; then
     mkdir -p "${year}_era${era}"
     for i in {0..7}; do
         echo "Stream $i"
-        cp templates/CRAB_template.py "${year}_era${era}/CRAD_stream_${i}.py"
+        cp templates/CRAB_template.py "${year}_era${era}/CRAB_stream_${i}.py"
         sed -i "s#YEAR#${year}#g" ${year}_era${era}/CRAD_stream_${i}.py
         sed -i "s#ERANAME#${era}#g" ${year}_era${era}/CRAD_stream_${i}.py
         sed -i "s#NUMBER#${i}#g" ${year}_era${era}/CRAD_stream_${i}.py
