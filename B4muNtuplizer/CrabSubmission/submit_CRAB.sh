1e9ffd1e60b8e4be667a0a1634ed2084dff649f7
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-11-29 10:53:33 +0100

    Update submit_CRAB.sh

--- B4muNtuplizer/CrabSubmission/submit_CRAB.sh
+++ B4muNtuplizer/CrabSubmission/submit_CRAB.sh
@@ -88,7 +88,7 @@ if [[ "$era" != *"MC"* ]]; then
         sed -i "s#DATASET_ID#${Data_ID[${i}]}#g" "${year}_era${era}/CRAB_stream_${i}.py"
         sed -i "s#FILE_TO_SUBMIT_PATH#${path}#g" "${year}_era${era}/CRAB_stream_${i}.py"
         sed -i "s#GOLDEN_JSON_PATH#${golden_json}#g" "${year}_era${era}/CRAB_stream_${i}.py"
-        crab submit -c ${year}_era${era}/CRAB_stream_${i}.py
+        crab submit -c "${year}_era${era}/CRAB_stream_${i}.py"
         echo "Stream $i submitted!"
         sleep 2
     done
@@ -106,7 +106,7 @@ else
         sed -i "s#MC_DATASET#${i}#g" "${year}_${era}/CRAB_MC_${j}.py"
         sed -i "s#FILE_TO_SUBMIT_PATH#${path}#g" "${year}_${era}/CRAB_MC_${j}.py"
         sed -i "s#INPUT_TYPE#${input_type}#g" "${year}_${era}/CRAB_MC_${j}.py"
-        crab submit -c ${year}_${era}/CRAB_MC_${j}.py
+        crab submit -c "${year}_${era}/CRAB_MC_${j}.py"
         echo "${era} - $j submitted!"
         ((j++))
         sleep 2
