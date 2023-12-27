922198ba1c741a250161a5b6103e20c0f986e0d2
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-12-27 22:59:38 +0100

    Update submit.sh

--- B4muNtuplizer/CrabSubmission/templates/submit.sh
+++ B4muNtuplizer/CrabSubmission/templates/submit.sh
@@ -1,15 +1,15 @@
-crab submit -c crab_DsTau3mu_reco2022_RunC_stream0_cfg.py
+crab submit -c CRAB_stream0_cfg.py
 sleep 5
-crab submit -c crab_DsTau3mu_reco2022_RunC_stream1_cfg.py
+crab submit -c CRAB_stream1_cfg.py
 sleep 5
-crab submit -c crab_DsTau3mu_reco2022_RunC_stream2_cfg.py
+crab submit -c CRAB_stream2_cfg.py
 sleep 5
-crab submit -c crab_DsTau3mu_reco2022_RunC_stream3_cfg.py
+crab submit -c CRAB_stream3_cfg.py
 sleep 5
-crab submit -c crab_DsTau3mu_reco2022_RunC_stream4_cfg.py
+crab submit -c CRAB_stream4_cfg.py
 sleep 5
-crab submit -c crab_DsTau3mu_reco2022_RunC_stream5_cfg.py
+crab submit -c CRAB_stream5_cfg.py
 sleep 5
-crab submit -c crab_DsTau3mu_reco2022_RunC_stream6_cfg.py
+crab submit -c CRAB_stream6_cfg.py
 sleep 5
-crab submit -c crab_DsTau3mu_reco2022_RunC_stream7_cfg.py
+crab submit -c CRAB_stream7_cfg.py
