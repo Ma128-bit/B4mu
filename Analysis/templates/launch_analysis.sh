e95d5acf93ffad57b7026a60936f684d7d3e5848
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-03 22:16:49 +0100

    Update launch_analysis.sh

--- Analysis/templates/launch_analysis.sh
+++ Analysis/templates/launch_analysis.sh
@@ -1,1 +1,16 @@
+# !/bin/sh
+# Usage:
+#    prepare_condor.sh <index>
+
+helpstring=" Usage:
+prepare_condor.sh [index] "
+index=$1
+
+# Check inputs
+if [ -z ${1+x} ]
+then
+echo -e ${helpstring}
+return
+fi
+
 source /cvmfs/sft.cern.ch/lcg/views/dev3/latest/x86_64-centos7-gcc11-opt/setup.sh
