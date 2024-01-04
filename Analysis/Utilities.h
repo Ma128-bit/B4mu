2120e7e4eef84e3b6481df4885f765537a7b8d5c
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-04 14:42:45 +0100

    Update Utilities.h

--- Analysis/Utilities.h
+++ Analysis/Utilities.h
@@ -84,19 +84,19 @@ int best_quadruplet(ROOT::VecOps::RVec<float> MuonPt, ROOT::VecOps::RVec<float> 
         }
         
         //Cut2 FlightDistBS_SV_Significance, dR and dz
-        //if(FlightDistBS_SV_Significance.at(j) < 2 ) continue;
+        if(FlightDistBS_SV_Significance.at(j) < 2 ) continue;
         vector<int> index = get_4index(MuonPt, Mu1_Pt.at(j), Mu2_Pt.at(j), Mu3_Pt.at(j), Mu4_Pt.at(j));
         if(index.at(0)==-1){
             cout<<"Error in index\n";
             continue;
         }
         
-        //if( !(isPairDeltaRGood(MuonEta, MuonPhi, index, 1)) ) continue;
+        if( !(isPairDeltaRGood(MuonEta, MuonPhi, index, 1)) ) continue;
         double vz1 = Muon_vz.at(index.at(0));
         double vz2 = Muon_vz.at(index.at(1));
         double vz3 = Muon_vz.at(index.at(2));
         double vz4 = Muon_vz.at(index.at(3));
-        //if( !(isPairDeltaZGood(vz1, vz2, vz3, vz4, 1) )) continue;
+        if( !(isPairDeltaZGood(vz1, vz2, vz3, vz4, 1) )) continue;
 
         //Cut3 isGlobal and isPF
         int isGlobal=0;
@@ -105,7 +105,7 @@ int best_quadruplet(ROOT::VecOps::RVec<float> MuonPt, ROOT::VecOps::RVec<float> 
             isGlobal = isGlobal + Muon_isGlobal.at(index.at(k));
             isPF = isPF + Muon_isPF.at(index.at(k));
         }
-        //if(isGlobal<2 || isPF<2) continue;
+        if(isGlobal<3 || isPF<3) continue;
         
         //Cut4 invariant mass
         if(!(Quadruplet_Mass.at(j)>5.15 && Quadruplet_Mass.at(j)<5.55)) continue;
