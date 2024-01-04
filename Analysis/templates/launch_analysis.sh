596daeb0de84a851b0b4b22a49929fffd2458ffe
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-04 12:33:37 +0100

    Update launch_analysis.sh

--- Analysis/templates/launch_analysis.sh
+++ Analysis/templates/launch_analysis.sh
@@ -12,7 +12,8 @@ if [ -z ${1+x} ]; then
   return
 fi
 
-source /cvmfs/sft.cern.ch/lcg/views/dev3/latest/x86_64-centos7-gcc11-opt/setup.sh
+#source /cvmfs/sft.cern.ch/lcg/views/dev3/latest/x86_64-centos7-gcc11-opt/setup.sh
 cd PATH
+cmsenv
 python3 Analizer.py --index $index --delta DELTAVAL --directory_IN INPUT_DIR --directory_OUT OUTPUT_DIR
 
