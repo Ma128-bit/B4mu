1dcce47e29883f6c465cc64a8cc770f0bf3f4d9d
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-04 13:42:26 +0100

    Create hadd_ALL.sh

---
+++ Analysis/hadd_ALL.sh
@@ -0,0 +1,36 @@
+# !/bin/sh
+# Usage:
+#    prepare_and_submit_ALL.sh <Year>
+
+helpstring="Usage:
+prepare_and_submit_ALL.sh [Year]"
+
+year=$1
+
+# Check inputs
+if [ -z ${1+x} ]; then
+    echo -e ${helpstring}
+    return
+fi
+
+declare -a Era_2022=("C" "D-v1" "D-v2" "E" "F" "G")
+declare -a Era_2023=("C-v1" "C-v2" "C-v3" "C-v4" "D-v1" "D-v2")
+
+if [ "${year}" == "2022" ]; then
+  eras=("${Era_2022[@]}")
+elif [ "${year}" == "2022" ]; then
+  eras=("${Era_2023[@]}")
+else
+  echo "Error: The year is incorrect."
+  return
+fi
+
+for e in "${eras[@]}"; do
+    cd "${year}_era${e}"
+    source hadd_era.sh
+    echo ""
+    cd ..
+    sleep 1
+done
+
+hadd "Analyzed_Data_${year}.root" "${year}_era*/Analyzed_Data_${year}_Era_*.root"
