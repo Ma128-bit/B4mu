c26bcae099f9549f78a3f2483c5ce3df0ab97900
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-04 11:55:15 +0100

    Update submit_era.sh

--- Analysis/templates/submit_era.sh
+++ Analysis/templates/submit_era.sh
@@ -4,6 +4,7 @@
 
 echo "Submit YEARNAME -- era ERANAME:"
 for i in {0..7}; do
+  echo -n "Stream ${i} "
   condor_submit -name ettore "stream_${i}/submit.condor"
 done
 echo "Done YEARNAME -- era ERANAME!"
