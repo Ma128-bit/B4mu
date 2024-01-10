15747bde4e1e7a524396605d50d95d0dccbda833
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-10 12:11:13 +0100

    Update Utilities.h

--- Analysis/Utilities.h
+++ Analysis/Utilities.h
@@ -106,7 +106,8 @@ vector<int> info_quadruplet(ROOT::VecOps::RVec<float> MuonPt, ROOT::VecOps::RVec
         }
         if(isGlobal<4 || isPF<4) continue;
         cont3++;
-        
+
+        /*
         //Cut5 HLT Trigger Matching
         vector<double> pt, eta, phi, pt_HLT, eta_HLT, phi_HLT;
         for(int h=0; h<index.size(); h++){
@@ -145,6 +146,7 @@ vector<int> info_quadruplet(ROOT::VecOps::RVec<float> MuonPt, ROOT::VecOps::RVec
         }
         
         if(HLT_matching<2) continue;
+        */
         cont4++;
         
         quad_indx.push_back(j);
