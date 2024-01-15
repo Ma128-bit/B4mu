239635e6bb09c7898950a3c6cf42c67ef55c896b
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-15 11:18:19 +0100

    Update Utilities_v2.h

--- Analysis/Utilities_v2.h
+++ Analysis/Utilities_v2.h
@@ -72,108 +72,41 @@ vector<int> get_4index(ROOT::VecOps::RVec<float> MuonPt, double pt1, double pt2,
     }
     return index;
 }
-vector<int> get_stat(ROOT::VecOps::RVec<float> MuonPt, ROOT::VecOps::RVec<float> MuonEta, ROOT::VecOps::RVec<float> MuonPhi, ROOT::VecOps::RVec<double> Mu1_Pt, ROOT::VecOps::RVec<double> Mu2_Pt, ROOT::VecOps::RVec<double> Mu3_Pt, ROOT::VecOps::RVec<double> Mu4_Pt, ROOT::VecOps::RVec<int> NGoodQuadruplets, ROOT::VecOps::RVec<double> QuadrupletVtx_Chi2, ROOT::VecOps::RVec<double> Quadruplet_Mass, ROOT::VecOps::RVec<double> Muon_isGlobal, ROOT::VecOps::RVec<double> Muon_isPF, ROOT::VecOps::RVec<double> Muon_isLoose, ROOT::VecOps::RVec<double> Muon_isMedium, ROOT::VecOps::RVec<double> Muon_isTight, ROOT::VecOps::RVec<double> Muon_isSoft, ROOT::VecOps::RVec<double> MuonPt_HLT, ROOT::VecOps::RVec<double> MuonEta_HLT, ROOT::VecOps::RVec<double> MuonPhi_HLT,  ROOT::VecOps::RVec<double> FlightDistBS_SV_Significance, ROOT::VecOps::RVec<double> Muon_vz){
-    int cont1=0, cont2=0, cont3=0, cont4=0;
+std::vector<std::vector<int>> get_stat(ROOT::VecOps::RVec<float> MuonPt, ROOT::VecOps::RVec<float> MuonEta, ROOT::VecOps::RVec<float> MuonPhi, ROOT::VecOps::RVec<double> Mu1_Pt, ROOT::VecOps::RVec<double> Mu2_Pt, ROOT::VecOps::RVec<double> Mu3_Pt, ROOT::VecOps::RVec<double> Mu4_Pt, ROOT::VecOps::RVec<int> NGoodQuadruplets, ROOT::VecOps::RVec<double> QuadrupletVtx_Chi2, ROOT::VecOps::RVec<double> Quadruplet_Mass, ROOT::VecOps::RVec<double> Muon_isGlobal, ROOT::VecOps::RVec<double> Muon_isPF, ROOT::VecOps::RVec<double> Muon_isLoose, ROOT::VecOps::RVec<double> Muon_isMedium, ROOT::VecOps::RVec<double> Muon_isTight, ROOT::VecOps::RVec<double> Muon_isSoft, ROOT::VecOps::RVec<double> MuonPt_HLT, ROOT::VecOps::RVec<double> MuonEta_HLT, ROOT::VecOps::RVec<double> MuonPhi_HLT,  ROOT::VecOps::RVec<double> FlightDistBS_SV_Significance, ROOT::VecOps::RVec<double> Muon_vz){
     vector<int> quad_indx;
-    for (int j=0; j<QuadrupletVtx_Chi2.size(); j++){
-        //Cut1 "strange" events
+    for (int j=0; j<Mu1_Pt.size(); j++){
         if(Mu1_Pt.at(j)==-99 || Mu2_Pt.at(j) == -99 || Mu3_Pt.at(j) == -99 || Mu4_Pt.at(j) == -99){ continue;}
-        
-        //Cut2 FlightDistBS_SV_Significance, dR and dz
-        //if(FlightDistBS_SV_Significance.at(j) < 2 ) continue;
         vector<int> index = get_4index(MuonPt, Mu1_Pt.at(j), Mu2_Pt.at(j), Mu3_Pt.at(j), Mu4_Pt.at(j));
         if(index.at(0)==-1){ cout<<"Error in index\n"; continue; }
-        cont1++;
-        
-        //if( !(isPairDeltaRGood(MuonEta, MuonPhi, index, 1)) ) continue;
-        double vz1 = Muon_vz.at(index.at(0));
-        double vz2 = Muon_vz.at(index.at(1));
-        double vz3 = Muon_vz.at(index.at(2));
-        double vz4 = Muon_vz.at(index.at(3));
-        //if( !(isPairDeltaZGood(vz1, vz2, vz3, vz4, 1) )) continue;
-        
-        //Cut3 invariant mass
-        if(!(Quadruplet_Mass.at(j)>5.05 && Quadruplet_Mass.at(j)<5.65)) continue;
-        cont2++;
-        
-        //Cut4 isGlobal and isPF
-        int isGlobal=0;
-        int isPF=0;
-        for(int k=0; k<index.size(); k++){
-            isGlobal = isGlobal + Muon_isGlobal.at(index.at(k));
-            isPF = isPF + Muon_isPF.at(index.at(k));
-        }
-        //if(isGlobal<4 || isPF<4) continue;
-        cont3++;
-
-        
-        //Cut5 HLT Trigger Matching
-        vector<double> pt_HLT, eta_HLT, phi_HLT;
-        vector<float> pt, eta, phi;
-        for(int h=0; h<index.size(); h++){
-            float pt_temp=MuonPt.at(index.at(h));
-            float eta_temp=MuonEta.at(index.at(h));
-            float phi_temp=MuonPhi.at(index.at(h));
-            pt.push_back(pt_temp);
-            eta.push_back(eta_temp);
-            phi.push_back(phi_temp);
-        }        
-        int HLT_matching = 0;
-        for(int w=0; w<MuonPt_HLT.size();w++){
-            for(int p=0; p<pt.size();p++){
-                double dphi = abs(phi.at(p) - MuonPhi_HLT.at(w));
-                double deta = abs(eta.at(p) - MuonEta_HLT.at(w));
-                if(dphi > double(M_PI)) dphi -= double(2*M_PI);
-                double dR = TMath::Sqrt(dphi*dphi + deta*deta);
-                double dpt = abs(pt.at(p) - MuonPt_HLT.at(w))/pt.at(p);
-                if(dR<0.03 && dpt<0.1){
-                    HLT_matching++;
-                    phi.erase(phi.begin() + p);
-                    eta.erase(eta.begin() + p);
-                    pt.erase(pt.begin() + p);
-                    break;
-                }
-            }
-        }
-        
-        if(HLT_matching<2) continue;
-        
-        cont4++;
-        
         quad_indx.push_back(j);
     }
-    vector<int> out;
-    if(quad_indx.size()==0) {out.push_back(-99); out.push_back(QuadrupletVtx_Chi2.size());
-    out.push_back(cont1); out.push_back(cont2); out.push_back(cont3); out.push_back(cont4); return out;}
+
+    std::vector<std::vector<std::vector<int>>> out;
+    if(quad_indx.size()==0) {quad_indx.push_back(-99); std::vector<std::vector<int>> temp; temp.push_back(quad_indx); out.push_back(temp); return out;}
+
+    std::vector<std::vector<int>> isGlobal(4);
+    std::vector<std::vector<int>> isPF(4);
+    std::vector<std::vector<int>> isLoose(4);
+    std::vector<std::vector<int>> isMedium(4);
+    std::vector<std::vector<int>> isMedium(4);
+    std::vector<std::vector<int>> isSoft(4);
     
-    double best_chi2=15000000000;
-    int best_i=-1;
-    for(int l=0; l<quad_indx.size(); l++){
-        double temp_i=quad_indx.at(l);
-        double temp_chi2 = QuadrupletVtx_Chi2.at(temp_i);
-        if(temp_chi2 < best_chi2){
-            best_chi2=temp_chi2;
-            best_i=temp_i;
+    for(int w=0; w<quad_indx.size(); w++){
+        vector<int> index = get_4index(MuonPt, Mu1_Pt.at(w), Mu2_Pt.at(w), Mu3_Pt.at(w), Mu4_Pt.at(w));
+        for(int k=0; k<index.size(); k++){
+            isGlobal[k].push_back(Muon_isGlobal.at(index.at(k)));
+            isPF[k].push_back(Muon_isPF.at(index.at(k)));
+            isLoose[k].push_back(Muon_isLoose.at(index.at(k)));
+            isMedium[k].push_back(Muon_isMedium.at(index.at(k)));
+            isTight[k].push_back(Muon_isTight.at(index.at(k)));
+            isSoft[k].push_back(Muon_isSoft.at(index.at(k)));
         }
     }
-    vector<int> index = get_4index(MuonPt, Mu1_Pt.at(best_i), Mu2_Pt.at(best_i), Mu3_Pt.at(best_i), Mu4_Pt.at(best_i));
-    int isGlobal=0, isPF=0, isLoose=0, isMedium=0, isTight=0, isSoft=0;
-    for(int k=0; k<index.size(); k++){
-        isGlobal = isGlobal + Muon_isGlobal.at(index.at(k));
-        isPF = isPF + Muon_isPF.at(index.at(k));
-        isLoose = isLoose + Muon_isLoose.at(index.at(k));
-        isMedium = isMedium + Muon_isMedium.at(index.at(k));
-        isTight = isTight + Muon_isTight.at(index.at(k));
-        isSoft = isSoft + Muon_isSoft.at(index.at(k));
-    }
-    out.push_back(best_i);
-    out.push_back(QuadrupletVtx_Chi2.size());
-    out.push_back(cont1); out.push_back(cont2); out.push_back(cont3); out.push_back(cont4);
     out.push_back(isGlobal);
     out.push_back(isPF);
     out.push_back(isLoose);
     out.push_back(isMedium);
-    out.push_back(isTight);
+    out.push_back(isMedium);
     out.push_back(isSoft);
     return out;
 }
