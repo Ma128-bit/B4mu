609026004bd997b6fe80e0a7ff54db4bd931695f
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-16 11:35:34 +0100

    Update Analizer_v2.py

--- Analysis/Analizer_v2.py
+++ Analysis/Analizer_v2.py
@@ -65,18 +65,18 @@ if __name__ == "__main__":
     print("Starting!")
     df = load_df(selected_files, "TreeMakerBkg/ntuple")
     #Find best Quadruplet
-    df = df.Define("Quadruplet_indexs","best_quadruplets(MuonPt, MuonEta, MuonPhi, Mu1_Pt, Mu2_Pt, Mu3_Pt, Mu4_Pt, NGoodQuadruplets, QuadrupletVtx_Chi2, Quadruplet_Mass, Muon_isGlobal, Muon_isPF, Muon_isLoose, Muon_isMedium, Muon_isTight, Muon_isSoft, MuonPt_HLT, MuonEta_HLT, MuonPhi_HLT, FlightDistBS_SV_Significance, Muon_vz)")
+    df = df.Define("isMC", add_index(isMC))
+    df = df.Define("Quadruplet_indexs","best_quadruplets(isMC, MuonPt, MuonEta, MuonPhi, Mu1_Pt, Mu2_Pt, Mu3_Pt, Mu4_Pt, NGoodQuadruplets, QuadrupletVtx_Chi2, Quadruplet_Mass, Muon_isGlobal, Muon_isPF, Muon_isLoose, Muon_isMedium, Muon_isTight, Muon_isSoft, MuonPt_HLT, MuonEta_HLT, MuonPhi_HLT, FlightDistBS_SV_Significance, Muon_vz)")
     df = df.Filter("Quadruplet_indexs[0]>-1")
 
     for chi in range(5):
         start_2 = time.time()
         branches=[]
+        branches.append("isMC")
         branches.append("Quadruplet_index")
         rdf = df.Define("Quadruplet_index", flat_index(chi), ["Quadruplet_indexs"])
         branches.append("chi2_label")
         rdf = rdf.Define("chi2_label", add_index(chi))
-        branches.append("isMC")
-        rdf = rdf.Define("isMC", add_index(isMC))
         
         rdf = rdf.Filter("Quadruplet_index>-1")
         rdf = rdf.Define("Stats","get_stat(Quadruplet_index, MuonPt, MuonEta, MuonPhi, Mu1_Pt, Mu2_Pt, Mu3_Pt, Mu4_Pt, NGoodQuadruplets, QuadrupletVtx_Chi2, Quadruplet_Mass, Muon_isGlobal, Muon_isPF, Muon_isLoose, Muon_isMedium, Muon_isTight, Muon_isSoft, Muon_isTrackerMuon, MuonPt_HLT, MuonEta_HLT, MuonPhi_HLT, FlightDistBS_SV_Significance, Muon_vz)")
