1c6104cb3ef0c20bcbcac586a0543d0ee32001e5
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-26 17:55:34 +0100

    Create MuonID_selection.py

---
+++ Utilities/MuonID_selection.py
@@ -0,0 +1,41 @@
+from ROOT import TFile, gROOT, gDirectory
+gROOT.SetBatch(True)
+import matplotlib.pyplot as plt
+import numpy as np
+import math, os, draw_utilities
+from tqdm import tqdm
+
+class ROOTDrawer(draw_utilities.ROOTDrawer):
+    pass
+    
+
+if __name__ == "__main__":
+    rdf_data = RDataFrame("FinalTree", "../Analysis/FinalFiles/Analyzed_Data_2022.root")
+    rdf_data = rdf_data.Filter("abs(Quadruplet_Mass-5.366)>0.15")
+    evt_data = rdf_data.Count();
+    rdf_MC = RDataFrame("FinalTree", "../Analysis/FinalFiles/Analyzed_Data_BsJPsiPhi.root")
+    evt_MC = rdf_MC.Count();
+    out_dir="AMS_plot"
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    muon_id = ["isGlobal", "isPF", "isLoose", "isMedium", "isTight", "isSoft", "isTracker"]
+
+    AMS = []
+    selections = []
+    for i in muon_id:
+        for j in muon_id:
+            for k in muon_id:
+                for w in muon_id:
+                    sel = i+"[0]+"+j+"[1]+"+k+"[2]+"+w+"[3] == 4"
+                    nbkg = rdf_data.Filter(sel).Count();
+                    nbkg = nbkg/evt_data
+                    nsig = rdf_MC.Filter(sel).Count();
+                    nsig = nsig/evt_MC
+                    AMS.append(math.sqrt(2*((nsig+nbkg)*math.log(1+nsig/nbkg) - nsig)))
+                    selections.append(sel)
+
+    best_sel = selections[AMS.index(max(AMS))]
+    print(best_sel)
+                    
+    
