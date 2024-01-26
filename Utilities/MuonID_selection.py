5d6b293cb6dec167f43feb712e1b99b61fba1c3f
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-26 21:56:21 +0100

    Update MuonID_selection.py

--- Utilities/MuonID_selection.py
+++ Utilities/MuonID_selection.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
 
     OUT: (isGlobal[0]+isGlobal[1]+isGlobal[2]+isGlobal[3] == 4) && (isSoft[0]+isSoft[1]+isSoft[2]+isSoft[3] == 4)
 
-    """
+    
     Nsel = len(muon_id)**2
     bar = Bar('Processing', max=Nsel)
     AMS = []
@@ -104,6 +104,9 @@ if __name__ == "__main__":
             selections.append(sel)
             bar.next()
 
+    OUT: (isMedium[0]+isMedium[1]+isMedium[2]+isMedium[3] == 4) || (isTight[0]+isTight[1]+isTight[2]+isTight[3] == 4)
+
+    """
     bar.finish()
     best_sel = selections[AMS.index(max(AMS))]
     print(best_sel)
