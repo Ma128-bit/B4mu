bb59e16613efb8f56d7ac6f7eb3c0352088c1717
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-26 16:58:59 +0100

    Update launch_analysis.sh

--- Analysis/templates/launch_analysis.sh
+++ Analysis/templates/launch_analysis.sh
@@ -16,5 +16,5 @@ fi
 cd PATH
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cmsenv
-python3 Analizer_v2.py --index $index --delta DELTAVAL --directory_IN INPUT_DIR --directory_OUT OUTPUT_DIR --isMC TRUEFALSE
+python3 Analizer.py --index $index --delta DELTAVAL --directory_IN INPUT_DIR --directory_OUT OUTPUT_DIR --isMC TRUEFALSE
 
