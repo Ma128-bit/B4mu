f06fb3321fb0d35cc6f062b06eb04f43b3c2f761
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-30 18:28:18 +0100

    Update Analizer.py

--- Analysis/Analizer.py
+++ Analysis/Analizer.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
     df = df.Define("Quadruplet_indexs","best_quadruplets(isMC, evt, MuonPt, MuonEta, MuonPhi, Mu1_Pt, Mu2_Pt, Mu3_Pt, Mu4_Pt, NGoodQuadruplets, QuadrupletVtx_Chi2, Quadruplet_Mass, Muon_isGlobal, Muon_isPF, Muon_isLoose, Muon_isMedium, Muon_isTight, Muon_isSoft, MuonPt_HLT, MuonEta_HLT, MuonPhi_HLT, FlightDistBS_SV_Significance, Muon_vz, GenParticle_Pt, GenParticle_Pt_v2, GenParticle_Eta_v2, GenParticle_Phi_v2, GenParticle_PdgId, GenParticle_MotherPdgId, GenParticle_GrandMotherPdgId)")
     df = df.Filter("Quadruplet_indexs[0]>-1")
 
-    for chi in range(5):
+    for chi in range(1):
         start_2 = time.time()
         branches=["evt", "isMC"]
         rdf = df.Define("Quadruplet_index", flat0D_int(chi), ["Quadruplet_indexs"])
