ed8807489c130c909453edc99c77bd7e62ffdfb5
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-30 12:01:40 +0100

    Update Utilities.h

--- Analysis/Utilities.h
+++ Analysis/Utilities.h
@@ -136,7 +136,7 @@ int GenMatching(ROOT::VecOps::RVec<float> MuonPt, ROOT::VecOps::RVec<float> Muon
     int Gen_matching = 0;
     for(int p=0; p<pt.size();p++){
         cout<<"Genpt: ";
-        for(int kk=0; kk<Genpt.size(); kk++) {cout<<Genpt[kk];}
+        for(int kk=0; kk<Genpt.size(); kk++) {cout<<Genpt[kk]<<" ";}
         cout<<endl;
         vector<double> dR_temp, dpt_temp;
         for(int w=0; w<Genpt.size();w++){
@@ -165,7 +165,6 @@ int GenMatching(ROOT::VecOps::RVec<float> MuonPt, ROOT::VecOps::RVec<float> Muon
 
 vector<int> best_quadruplets(int isMC, int evt, ROOT::VecOps::RVec<float> MuonPt, ROOT::VecOps::RVec<float> MuonEta, ROOT::VecOps::RVec<float> MuonPhi, ROOT::VecOps::RVec<double> Mu1_Pt, ROOT::VecOps::RVec<double> Mu2_Pt, ROOT::VecOps::RVec<double> Mu3_Pt, ROOT::VecOps::RVec<double> Mu4_Pt, ROOT::VecOps::RVec<int> NGoodQuadruplets, ROOT::VecOps::RVec<double> QuadrupletVtx_Chi2, ROOT::VecOps::RVec<double> Quadruplet_Mass, ROOT::VecOps::RVec<double> Muon_isGlobal, ROOT::VecOps::RVec<double> Muon_isPF, ROOT::VecOps::RVec<double> Muon_isLoose, ROOT::VecOps::RVec<double> Muon_isMedium, ROOT::VecOps::RVec<double> Muon_isTight, ROOT::VecOps::RVec<double> Muon_isSoft, ROOT::VecOps::RVec<double> MuonPt_HLT, ROOT::VecOps::RVec<double> MuonEta_HLT, ROOT::VecOps::RVec<double> MuonPhi_HLT,  ROOT::VecOps::RVec<double> FlightDistBS_SV_Significance, ROOT::VecOps::RVec<double> Muon_vz, ROOT::VecOps::RVec<double> GenParticle_Pt, ROOT::VecOps::RVec<double> GenParticle_Pt_v2, ROOT::VecOps::RVec<double> GenParticle_Eta_v2, ROOT::VecOps::RVec<double> GenParticle_Phi_v2,  ROOT::VecOps::RVec<int> GenParticle_PdgId, ROOT::VecOps::RVec<int> GenParticle_MotherPdgId, ROOT::VecOps::RVec<int> GenParticle_GrandMotherPdgId){
     vector<int> quad_indx;
-    vector<int> genmetch_id;
     for (int j=0; j<QuadrupletVtx_Chi2.size(); j++){
         //Cut1 "strange" events
         if(Mu1_Pt.at(j)==-99 || Mu2_Pt.at(j) == -99 || Mu3_Pt.at(j) == -99 || Mu4_Pt.at(j) == -99){ continue;}
@@ -239,7 +238,6 @@ vector<int> best_quadruplets(int isMC, int evt, ROOT::VecOps::RVec<float> MuonPt
         if(isMC>0){
             int genmatch = GenMatching(MuonPt, MuonEta, MuonPhi, Mu1_Pt.at(j), Mu2_Pt.at(j), Mu3_Pt.at(j), Mu4_Pt.at(j), GenParticle_Pt, GenParticle_Pt_v2, GenParticle_Eta_v2, GenParticle_Phi_v2, GenParticle_PdgId, GenParticle_MotherPdgId, GenParticle_GrandMotherPdgId);
             if(genmatch!=1) continue;
-            genmetch_id.push_back(genmatch);
         }
 
         quad_indx.push_back(j);
@@ -255,7 +253,6 @@ vector<int> best_quadruplets(int isMC, int evt, ROOT::VecOps::RVec<float> MuonPt
         cout<<endl;
         cout<<"Quadruplets:"<<endl;
         for(int ww=0; ww<quad_indx.size(); ww++){
-            cout<<"genmatch_id: "<<genmetch_id[ww]<<endl;
             vector<int> index_temp = get_4index(MuonPt, Mu1_Pt.at(ww), Mu2_Pt.at(ww), Mu3_Pt.at(ww), Mu4_Pt.at(ww));
             for(int www=0;www<index_temp.size();www++){
                 cout<<MuonPt.at(index_temp.at(www))<<" ";
