46f0f39dbf9a5db61aeb0e9d10f520f768c6cde7
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-30 12:23:51 +0100

    Update Utilities.h

--- Analysis/Utilities.h
+++ Analysis/Utilities.h
@@ -155,7 +155,7 @@ int GenMatching(ROOT::VecOps::RVec<float> MuonPt, ROOT::VecOps::RVec<float> Muon
         double dRpt_min = *dRpt_min_p;
         double dpt_min = dpt_temp[dRpt_minID];
         double dR_min = dR_temp[dRpt_minID];
-        if(dR_min<0.03 && dpt_min<0.07){
+        if(dR_min<0.03 && dpt_min<0.08){
             Gen_matching++;
             Genpt.erase(Genpt.begin() + dRpt_minID);
             Geneta.erase(Geneta.begin() + dRpt_minID);
