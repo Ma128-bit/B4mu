94fec6fa7bb71415d61eedb1818fb11249827c3c
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-02-09 18:22:56 +0100

    Update B4MuSkim_miniAOD_cff.py

--- B4muNtuplizer/SkimTools/python/B4MuSkim_miniAOD_cff.py
+++ B4muNtuplizer/SkimTools/python/B4MuSkim_miniAOD_cff.py
@@ -33,7 +33,7 @@ PatMuons = patMuons.clone(
 looseMuons = cms.EDFilter("PATMuonSelector",
                           src = cms.InputTag("slimmedMuons"),
                           #cut = cms.string('pt > 2 &&  abs(eta)<2.4 && (innerTrack().isNonnull) && (charge!=0) && (innerTrack().hitPattern().numberOfValidPixelHits()>0)'),
-                          cut = cms.string('pt > 2 &&  abs(eta)<2.4 && (charge!=0)'),
+                          cut = cms.string('pt > 0 &&  abs(eta)<5 && (innerTrack().isNonnull) && (charge!=0) && (innerTrack().hitPattern().numberOfValidPixelHits()>0)'),
                           filter = cms.bool(True)
 )
 
