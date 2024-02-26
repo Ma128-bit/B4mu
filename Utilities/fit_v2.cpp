7e4d02e8db26d6e0d1ee30b78d30a1856f91b1f5
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-02-26 18:11:10 +0100

    Update fit_v2.cpp

--- Utilities/fit_v2.cpp
+++ Utilities/fit_v2.cpp
@@ -31,14 +31,14 @@ void fit_v2() {
     }
     
     //tree->Draw("Quadruplet_Mass>>h1(24,5.05,5.65)","((BsJPsiPhi_sel_OS1>0 && Dimu_OS1_dR>0.17 && Dimu_OS1_dR<1.08) || (BsJPsiPhi_sel_OS2>0 && Dimu_OS2_dR>0.17 && Dimu_OS2_dR<1.08)) && FlightDistBS_SV_Significance>2.25 ");
-    tree->Draw("Quadruplet_Mass_eq>>h1(52,5.0, 6.0)","(isJPsiPhi==1)");
+    tree->Draw("Quadruplet_Mass_eq>>h1(100,5.0, 6.0)","(isJPsiPhi==1)");
     TH1F *h1 = (TH1F*)gDirectory->Get("h1");
     
     RooRealVar x("Quadruplet_Mass_eq", "Quadruplet_Mass_eq", 5.0, 6.0);
     x.setRange("R1", 5.0, 5.25);
     x.setRange("R2", 5.55, 6.0);
     x.setRange("RT", 5.0, 6.0);
-    x.setBins(52);
+    x.setBins(100);
     
     RooDataHist data("data", h1->GetTitle(), RooArgSet(x), Import(*h1, kFALSE));
     
