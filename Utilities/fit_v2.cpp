1371726c4ab42b4508955bc2b34ba99c5f6840d2
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-02-28 18:14:33 +0100

    Update fit_v2.cpp

--- Utilities/fit_v2.cpp
+++ Utilities/fit_v2.cpp
@@ -23,7 +23,7 @@ void fit_v2() {
     }
 
     // Ottenere l'albero dal file
-    TTree *tree = (TTree*)file->Get("OutputTree");
+    TTree *tree = (TTree*)file->Get("FinalTree");
     if (!tree) {
         std::cerr << "Errore nell'apertura dell'albero" << std::endl;
         file->Close();
