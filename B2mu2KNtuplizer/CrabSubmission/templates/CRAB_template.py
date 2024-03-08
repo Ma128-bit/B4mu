28e12068020c79d512d570034c22abf564c8f2cc
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-03-08 17:44:01 +0100

    Update CRAB_template.py

--- B2mu2KNtuplizer/CrabSubmission/templates/CRAB_template.py
+++ B2mu2KNtuplizer/CrabSubmission/templates/CRAB_template.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config, getUsername
 config = config()
 
-config.General.requestName = 'SkimB4Mu_YEAReraERANAME_streamNUMBER_Mini'
+config.General.requestName = 'SkimB2Mu2K_YEAReraERANAME_streamNUMBER_Mini'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
@@ -17,7 +17,7 @@ config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 50
 config.Data.lumiMask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/GOLDEN_JSON_PATH'
 #config.Data.publication = True
-config.Data.outputDatasetTag = 'SkimB4Mu_YEAReraERANAME_streamNUMBER_Mini'
+config.Data.outputDatasetTag = 'SkimB2Mu2K_YEAReraERANAME_streamNUMBER_Mini'
 config.JobType.allowUndistributedCMSSW = True 
 config.Site.storageSite = 'T2_IT_Bari'
 config.Site.ignoreGlobalBlacklist  = True
