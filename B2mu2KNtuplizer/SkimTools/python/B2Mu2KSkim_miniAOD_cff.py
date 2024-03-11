57303b047cfee18bca66a19114d354120ebed283
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-03-11 11:16:49 +0100

    Update B2Mu2KSkim_miniAOD_cff.py

--- B2mu2KNtuplizer/SkimTools/python/B2Mu2KSkim_miniAOD_cff.py
+++ B2mu2KNtuplizer/SkimTools/python/B2Mu2KSkim_miniAOD_cff.py
@@ -55,7 +55,7 @@ LooseTrackCandidate = cms.EDProducer("TrackFromCandProducer",
 
 RecoTrackCand = cms.EDProducer("ConcreteChargedCandidateProducer",
                                 src = cms.InputTag("LooseTrackCandidate"),
-                                particleType = cms.string("pi+"),
+                                particleType = cms.string("K+"),
 )
 
 DiKCand  = cms.EDProducer("CandViewShallowCloneCombiner",
