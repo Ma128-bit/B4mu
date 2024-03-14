76790fc75ae72e17dcd9bfb9c16c386392911523
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-03-14 17:00:40 +0100

    Update Analizer.py

--- Analysis/Analizer.py
+++ Analysis/Analizer.py
@@ -224,8 +224,8 @@ if __name__ == "__main__":
             rdf = DiMassVar_control(rdf, branches, analysis_type)
             branches.append("PhiMassTest2K")
             branches.append("PhiMassTestKpi")
-            rdf = rdf.Define("PhiMassTest2K", TwoObjMassFit(0.493677, 0.493677), ["RefTrack3_Pt", "RefTrack4_Pt", "RefTrack3_Eta", "RefTrack4_Eta","RefTrack3_Phi", "RefTrack4_Phi"])
-            rdf = rdf.Define("PhiMassTestKpi", TwoObjMassFit(0.493677, 0.139570), ["RefTrack3_Pt", "RefTrack4_Pt", "RefTrack3_Eta", "RefTrack4_Eta","RefTrack3_Phi", "RefTrack4_Phi"])
+            #rdf = rdf.Define("PhiMassTest2K", TwoObjMassFit(0.493677, 0.493677), ["RefTrack3_Pt", "RefTrack4_Pt", "RefTrack3_Eta", "RefTrack4_Eta","RefTrack3_Phi", "RefTrack4_Phi"])
+            #rdf = rdf.Define("PhiMassTestKpi", TwoObjMassFit(0.493677, 0.139570), ["RefTrack3_Pt", "RefTrack4_Pt", "RefTrack3_Eta", "RefTrack4_Eta","RefTrack3_Phi", "RefTrack4_Phi"])
             
         if not output_dir.endswith("/"):
             output_dir= output_dir + "/"
