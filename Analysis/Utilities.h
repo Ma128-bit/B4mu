f425da296cf1a1e596e7ba11dd4867994f978bbb
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-03-14 12:51:03 +0100

    Update Utilities.h

--- Analysis/Utilities.h
+++ Analysis/Utilities.h
@@ -519,6 +519,7 @@ double flattening(ROOT::VecOps::RVec<double> var, int Quadruplet_index){
     }
     return value;
 }
+
 std::pair<std::vector<std::vector<int>>, std::vector<std::vector<int>>> Dimuon(double Mu1_Pt, double Mu2_Pt, double Mu3_Pt, double Mu4_Pt, ROOT::VecOps::RVec<float> MuonPt, ROOT::VecOps::RVec<float> MuonEta, ROOT::VecOps::RVec<float> MuonPhi, ROOT::VecOps::RVec<double> MuonCharge){
     vector<int> index = get_4index(MuonPt, Mu1_Pt, Mu2_Pt, Mu3_Pt, Mu4_Pt);
     //if(index.at(0)==-1) return 0;
@@ -794,7 +795,7 @@ int BsJPsiPhi(double Dimu_OS_max, double Dimu_OS_min){
     else return 0;
 }
 
-double not_refit_mass(ROOT::VecOps::RVec<float> MuonPt, double pt1, double pt2, double pt3, double pt4,  ROOT::VecOps::RVec<float> MuonEta, ROOT::VecOps::RVec<float> MuonPhi, ROOT::VecOps::RVec<double> MuonEnergy){
+double NoRefitMassB4mu(ROOT::VecOps::RVec<float> MuonPt, double pt1, double pt2, double pt3, double pt4, double eta3, double eta4, double phi3, double phi4, ROOT::VecOps::RVec<float> MuonEta, ROOT::VecOps::RVec<float> MuonPhi, ROOT::VecOps::RVec<double> MuonEnergy){
     vector<int> index = get_4index(MuonPt, pt1, pt2, pt3, pt4);
     vector<double> eta, phi, en;
     for(int i=0; i<index.size();i++){
@@ -807,6 +808,40 @@ double not_refit_mass(ROOT::VecOps::RVec<float> MuonPt, double pt1, double pt2, 
     mu2.SetPtEtaPhiE(pt2, eta[1], phi[1], en[1]);
     mu3.SetPtEtaPhiE(pt3, eta[2], phi[2], en[2]);
     mu4.SetPtEtaPhiE(pt4, eta[3], phi[3], en[3]);
+    mutot = mu1 + mu2 + mu3 + mu4;
+    return mutot.M();
+}
+
+double NoRefitMassB2mu2K(ROOT::VecOps::RVec<float> MuonPt, double pt1, double pt2, double pt3, double pt4, double eta3, double eta4, double phi3, double phi4,  ROOT::VecOps::RVec<float> MuonEta, ROOT::VecOps::RVec<float> MuonPhi, ROOT::VecOps::RVec<double> MuonEnergy){
+    vector<int> index = get_2index(MuonPt, pt1, pt2);
+    vector<double> eta, phi, en;
+    for(int i=0; i<index.size();i++){
+        eta.push_back(MuonEta.at(index.at(i)));
+        phi.push_back(MuonPhi.at(index.at(i)));
+        en.push_back(MuonEnergy.at(index.at(i)));
+    }
+    TLorentzVector mu1, mu2, mu3, mu4, mutot;
+    mu1.SetPtEtaPhiE(pt1, eta[0], phi[0], en[0]);
+    mu2.SetPtEtaPhiE(pt2, eta[1], phi[1], en[1]);
+    mu3.SetPtEtaPhiM(pt3, eta3, phi3, 0.493677);
+    mu4.SetPtEtaPhiM(pt4, eta4, phi4, 0.493677);
+    mutot = mu1 + mu2 + mu3 + mu4;
+    return mutot.M();
+}
+
+double NoRefitMassB2muKpi(ROOT::VecOps::RVec<float> MuonPt, double pt1, double pt2, double pt3, double pt4, double eta3, double eta4, double phi3, double phi4, ROOT::VecOps::RVec<float> MuonEta, ROOT::VecOps::RVec<float> MuonPhi, ROOT::VecOps::RVec<double> MuonEnergy){
+    vector<int> index = get_2index(MuonPt, pt1, pt2);
+    vector<double> eta, phi, en;
+    for(int i=0; i<index.size();i++){
+        eta.push_back(MuonEta.at(index.at(i)));
+        phi.push_back(MuonPhi.at(index.at(i)));
+        en.push_back(MuonEnergy.at(index.at(i)));
+    }
+    TLorentzVector mu1, mu2, mu3, mu4, mutot;
+    mu1.SetPtEtaPhiE(pt1, eta[0], phi[0], en[0]);
+    mu2.SetPtEtaPhiE(pt2, eta[1], phi[1], en[1]);
+    mu3.SetPtEtaPhiE(pt3, eta3, phi3, 0.493677);
+    mu4.SetPtEtaPhiE(pt4, eta4, phi4, 0.139570);
     mutot = mu1 + mu2 + mu3 + mu4;
     return mutot.M();
 }
