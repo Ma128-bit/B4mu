b334b623bb5a71075c51c075369c01956dc74465
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-03-14 16:37:44 +0100

    Update Analizer.py

--- Analysis/Analizer.py
+++ Analysis/Analizer.py
@@ -222,6 +222,10 @@ if __name__ == "__main__":
 
         if(analysis_type!="B4mu"):
             rdf = DiMassVar_control(rdf, branches, analysis_type)
+            branches.append("PhiMassTest2K")
+            branches.append("PhiMassTestKpi")
+            rdf = rdf.Define("PhiMassTest2K", TwoObjMassFit(0.493677, 0.493677), ["RefTrac3_Pt", "RefTrac4_Pt", "RefTrac3_Eta", "RefTrac4_Eta","RefTrac3_Phi", "RefTrac4_Phi"])
+            rdf = rdf.Define("PhiMassTestKpi", TwoObjMassFit(0.493677, 0.139570), ["RefTrac3_Pt", "RefTrac4_Pt", "RefTrac3_Eta", "RefTrac4_Eta","RefTrac3_Phi", "RefTrac4_Phi"])
             
         if not output_dir.endswith("/"):
             output_dir= output_dir + "/"
