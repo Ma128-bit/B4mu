6fa99acd11cb5a2501ad2cb09d4758c435fc0098
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-03-19 15:08:03 +0100

    Update ParticleDecayDrawer_example.py

--- B2mu2KNtuplizer/SkimTools/test/ParticleDecayDrawer_example.py
+++ B2mu2KNtuplizer/SkimTools/test/ParticleDecayDrawer_example.py
@@ -9,7 +9,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(5) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        'root://xrootd-cms.infn.it//store/mc/Run3Summer22EEMiniAODv4/BdToJpsiKstar_BMuonFilter_SoftQCDnonD_TuneCP5_13p6TeV_pythia8-evtgen/MINIAODSIM/130X_mcRun3_2022_realistic_postEE_v6-v2/2520000/9bcde9fb-d7dc-461f-8529-6d7af0cc4ae5.root'
+        'root://xrootd-cms.infn.it//store/mc/Run3Summer22EEMiniAODv4/BdtoKstarPsi2s_Psi2sto2Mu_KstartoKPi_TuneCP5_13p6TeV_pythia8-evtgen/MINIAODSIM/130X_mcRun3_2022_realistic_postEE_v6-v2/2520000/e36e8f4d-9533-4122-8334-2c25e68601fd.root'
         #'root://xrootd-cms.infn.it//store/mc/Run3Summer22EEMiniAODv4/BdtoJpsiKstar_Jpsito2Mu_KstartoKPi_TuneCP5_13p6TeV_pythia8-evtgen/MINIAODSIM/130X_mcRun3_2022_realistic_postEE_v6-v2/40000/c741a6b8-14e4-4937-9547-5667b9c76483.root'
     ),
 )
