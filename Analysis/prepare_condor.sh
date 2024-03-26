846dd831530e66a49c68156251c857ec781c3528
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-03-26 15:14:08 +0100

    Update prepare_condor.sh

--- Analysis/prepare_condor.sh
+++ Analysis/prepare_condor.sh
@@ -35,6 +35,7 @@ declare -a E_2022=("240222_114640" "240222_114709" "240222_114736" "240222_11480
 declare -a F_2022=("240221_222028" "240221_222042" "240221_222058" "240222_111910" "240222_102944" "240221_222149" "240221_222204" "240221_222219")
 declare -a G_2022=("240221_222246" "240221_222308" "240221_222324" "240221_222340" "240221_222356" "240221_222410" "240221_222426" "240221_222442")
 
+declare -a B_2023=()
 declare -a C_v1_2023=("240221_222514" "240221_222529" "240222_103359" "240221_222557" "240221_222615" "240221_222633" "240221_222647" "240221_222702")
 declare -a C_v2_2023=("240221_222723" "240221_222740" "240221_222757" "240221_222813" "240221_222827" "240221_222844" "240221_222858" "240221_222912")
 declare -a C_v3_2023=("240221_222957" "240221_223012" "240221_223027" "240221_223042" "240221_223058" "240221_223116" "240221_223130" "240221_223145")
@@ -46,8 +47,23 @@ declare -a MC22_B4mu_pre=("BdTo4Mu_FourMuonFilter_TuneCP5_13p6TeV_pythia8-evtgen
 declare -a MC22_B4mu_post=("BdTo4Mu_FourMuonFilter_TuneCP5_13p6TeV_pythia8-evtgen/SkimB4Mu_2022_MC_B4mu_post_Bd_Mini/240229_131939" "Bs0To4Mu_FourMuonFilter_TuneCP5_13p6TeV_pythia8-evtgen/SkimB4Mu_2022_MC_B4mu_post_Bs_Mini/240229_131952" "BsToJpsiPhi_JMM_PhiMM_MuFilter_SoftQCDnonD_TuneCP5_13p6TeV-pythia8-evtgen/SkimB4Mu_2022_MC_BsJPsiPhi_post_BsJPsiPhi_Mini/240221_224937")
 declare -a B4mu_MC_label=("Bd_4mu" "Bs_4mu" "BsJPsiPhi")
 
-declare -a MC22_B2mu2trk_pre=("BstoJpsiPhi_Jpsito2Mu_Phito2K_MuFilter_TuneCP5_13p6TeV_pythia8-evtgen/SkimB2Mu2K_2022_MC_B2mu2trk_pre_Bs2mu2K_Mini/240319_161554" "BdtoJpsiKstar_Jpsito2Mu_KstartoKPi_MuFilter_TuneCP5_13p6TeV_pythia8-evtgen/SkimB2Mu2K_2022_MC_B2mu2trk_pre_Bd2muKpi_Mini/240319_161608")
-declare -a MC22_B2mu2trk_post=("BstoJpsiPhi_Jpsito2Mu_Phito2K_MuFilter_TuneCP5_13p6TeV_pythia8-evtgen/SkimB2Mu2K_2022_MC_B2mu2trk_post_Bs2mu2K_Mini/240311_160529" "BdtoJpsiKstar_Jpsito2Mu_KstartoKPi_MuFilter_TuneCP5_13p6TeV_pythia8-evtgen/SkimB2Mu2K_2022_MC_B2mu2trk_post_Bd2muKpi_Mini/240311_160548")
+declare -a Control_C_2022=("240325_150034" "240325_150048" "240325_150101" "240325_150115" "240325_150132" "240325_150147" "240325_150201" "240325_150215")
+declare -a Control_D_v1_2022=("240325_150236" "240325_150251" "240325_150305" "240325_150320" "240325_150334" "240325_150350" "240325_150404" "240326_115106")
+declare -a Control_D_v2_2022=("240325_150555" "240325_150613" "240325_150630" "240325_150647" "240325_150706" "240325_150725" "240325_150743" "240325_150759")
+declare -a Control_E_2022=("240325_150848" "240325_150904" "240325_150921" "240325_150938" "240325_150955" "240325_151011" "240325_151026" "240325_151042")
+declare -a Control_F_2022=("240325_140458" "240325_140510" "240325_140523" "240325_140538" "240325_140553" "240325_140608" "240325_140622" "240325_140635")
+declare -a Control_G_2022=("240325_140819" "240325_140832" "240325_140845" "240325_140859" "240325_140912" "240325_140924" "240325_140937" "240325_140950")
+
+declare -a Control_B_2023=("240325_123033" "240325_123051" "240325_123106" "240325_123121" "240325_123136" "240325_123154" "240325_123215" "240325_123235")
+declare -a Control_C_v1_2023=("240325_125312" "240325_125326" "240325_125339" "240325_125352" "240325_125405" "240325_125420" "240325_125434" "240325_125448")
+declare -a Control_C_v2_2023=("240326_114255" "240325_125607" "240325_125619" "240325_125631" "240325_125643" "240325_125656" "240325_125707" "240325_125719")
+declare -a Control_C_v3_2023=("240326_114422" "240325_125840" "240325_125853" "240325_125907" "240325_125920" "240325_125933" "240325_125946" "240325_125958")
+declare -a Control_C_v4_2023=("240325_130127" "240325_130140" "240325_130153" "240325_130205" "240325_130218" "240325_130231" "240325_130246" "240325_130300")
+declare -a Control_D_v1_2023=("240325_132534" "240325_132546" "240326_114706" "240325_132611" "240325_132623" "240325_132636" "240325_132649" "240325_132701")
+declare -a Control_D_v2_2023=("240325_132915" "240325_132927" "240325_132939" "240325_132959" "240325_133013" "240326_114854" "240325_133038" "240325_133051")
+
+declare -a MC22_B2mu2trk_pre=("BstoJpsiPhi_Jpsito2Mu_Phito2K_MuFilter_TuneCP5_13p6TeV_pythia8-evtgen/SkimB2Mu2K_2022_MC_B2mu2trk_pre_Bs2mu2K_Mini/240321_102408" "BdtoJpsiKstar_Jpsito2Mu_KstartoKPi_MuFilter_TuneCP5_13p6TeV_pythia8-evtgen/SkimB2Mu2K_2022_MC_B2mu2trk_pre_Bd2muKpi_Mini/240321_102520")
+declare -a MC22_B2mu2trk_post=("BstoJpsiPhi_Jpsito2Mu_Phito2K_MuFilter_TuneCP5_13p6TeV_pythia8-evtgen/SkimB2Mu2K_2022_MC_B2mu2trk_post_Bs2mu2K_Mini/240325_102401" "BdtoJpsiKstar_Jpsito2Mu_KstartoKPi_MuFilter_TuneCP5_13p6TeV_pythia8-evtgen/SkimB2Mu2K_2022_MC_B2mu2trk_post_Bd2muKpi_Mini/240325_102412")
 declare -a B2mu2trk_MC_label=("B2mu2K" "B2muKpi")
 
 if [ "${Ana_temp}" == "B4mu" ]; then
@@ -86,6 +102,9 @@ if [ "${Ana_temp}" == "B4mu" ]; then
         esac
     elif [ "${year}" == "2023" ]; then
         case "$era" in
+          B)
+            datasets=("${B_2023[@]}")
+            ;;
           C-v1)
             datasets=("${C_v1_2023[@]}")
             ;;
@@ -117,22 +136,22 @@ elif [ "${Ana_temp}" == "B2mu2trk" ]; then
     if [ "${year}" == "2022" ]; then
         case "$era" in
           C)
-            datasets=("${C_2022[@]}")
+            datasets=("${Control_C_2022[@]}")
             ;;
           D-v1)
-            datasets=("${D_v1_2022[@]}")
+            datasets=("${Control_D_v1_2022[@]}")
             ;;
           D-v2)
-            datasets=("${D_v2_2022[@]}")
+            datasets=("${Control_D_v2_2022[@]}")
             ;;
           E)
-            datasets=("${E_2022[@]}")
+            datasets=("${Control_E_2022[@]}")
             ;;
           F)
-            datasets=("${F_2022[@]}")
+            datasets=("${Control_F_2022[@]}")
             ;;
           G)
-            datasets=("${G_2022[@]}")
+            datasets=("${Control_G_2022[@]}")
             ;;
           MC_B2mu2trk_pre)
             datasets=("${MC22_B2mu2trk_pre[@]}")
@@ -149,23 +168,26 @@ elif [ "${Ana_temp}" == "B2mu2trk" ]; then
         esac
     elif [ "${year}" == "2023" ]; then
         case "$era" in
+          B)
+            datasets=("${Control_B_2023[@]}")
+            ;;
           C-v1)
-            datasets=("${C_v1_2023[@]}")
+            datasets=("${Control_C_v1_2023[@]}")
             ;;
           C-v2)
-            datasets=("${C_v2_2023[@]}")
+            datasets=("${Control_C_v2_2023[@]}")
             ;;
           C-v3)
-            datasets=("${C_v3_2023[@]}")
+            datasets=("${Control_C_v3_2023[@]}")
             ;;
           C-v4)
-            datasets=("${C_v4_2023[@]}")
+            datasets=("${Control_C_v4_2023[@]}")
             ;;
           D-v1)
-            datasets=("${D_v1_2023[@]}")
+            datasets=("${Control_D_v1_2023[@]}")
             ;;
           D-v2)
-            datasets=("${D_v2_2023[@]}")
+            datasets=("${Control_D_v2_2023[@]}")
             ;;
           *)
             echo "Error: The era is incorrect."
