1ec57a322acf07129b15dc3c87466db2c66bb74c
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-04-09 14:07:16 +0200

    Update Fit_B2mu.cpp

--- Utilities/Fit_B2mu.cpp
+++ Utilities/Fit_B2mu.cpp
@@ -193,7 +193,8 @@ void Fit2muKpiM(TString dataFile="../Analysis/FinalFiles_B2muKpi/Analyzed_Data_B
     RooRealVar c2("c2", "c2", -0.2, -10, 10);
     RooRealVar c3("c3", "c3", -0.2, -10, 10);
     
-    RooExponential pol_bkg("pol_bkg", "pol_bkg", x, c1);
+    //RooExponential pol_bkg("pol_bkg", "pol_bkg", x, c1);
+    RooChebychev pol_bkg("pol_bkg", "pol_bkg", x, RooArgList(c1,c2));
     pol_bkg.fitTo(data,Range("R1,R3"));
     
     // Creare la gaussiana
@@ -204,7 +205,7 @@ void Fit2muKpiM(TString dataFile="../Analysis/FinalFiles_B2muKpi/Analyzed_Data_B
     // Creare la gaussiana
     RooRealVar mean2("mean2", "Media gaussiana2", (up+down)/2, down, up);
     RooRealVar sigma2("sigma2", "Deviazione standard gaussiana2", 0.05, 0.001, 0.3);
-    RooGaussian voigt_pdf2("voigt_pdf2", "Signal Gaussian PDF2", x, mean2, sigma2);
+    RooGaussian voigt_pdf2("voigt_pdf2", "Signal Gaussian PDF2", x, mean, sigma2);
     
     // Creare il modello di fit combinando fondo e gaussiana
     RooRealVar nsig("nsig", "Numero di segnali", 0.25e+07, 1e+05, 1e+07);
@@ -218,7 +219,77 @@ void Fit2muKpiM(TString dataFile="../Analysis/FinalFiles_B2muKpi/Analyzed_Data_B
     RooPlot *frame = x.frame();
     data.plotOn(frame);
     model.plotOn(frame, Components(voigt_pdf, voigt_pdf2), LineStyle(kDashed), LineColor(kRed));
-    model.paramOn(frame, Parameters(RooArgSet(nsig, nbkg, mean, sigma)), Layout(0.5,0.9,0.9));
+    model.paramOn(frame, Parameters(RooArgSet(nsig, nsig2, nbkg, mean, sigma)), Layout(0.1,0.4,0.9));
+    model.plotOn(frame, Components(pol_bkg), LineStyle(kDashed), LineColor(kGreen));
+    model.plotOn(frame);
+    
+    TCanvas *canvas = new TCanvas("canvas", "Fit Result", 900, 600);
+    frame->Draw();
+    //canvas->SaveAs("Fit_results/Fit_BsJPsiPhi.png");
+
+    // Chiudere il file
+    //file->Close();
+}
+
+void Fit2mu2KM(TString dataFile="../Analysis/FinalFiles_B2muKpi/Analyzed_Data_B2muKpi_2022.root", TString var="Quadruplet_Mass", float down=4.9, float up=5.6) {
+    // Aprire il file root contenente l'albero
+    TFile *file = new TFile(dataFile);
+    if (!file || file->IsZombie()) {
+        std::cerr << "Errore nell'apertura del file" << std::endl;
+        return;
+    }
+
+    // Ottenere l'albero dal file
+    TTree *tree = (TTree*)file->Get("FinalTree");
+    if (!tree) {
+        std::cerr << "Errore nell'apertura dell'albero" << std::endl;
+        file->Close();
+        return;
+    }
+    TString s;
+    s.Form(">>h1(100,%f,%f)", down, up);
+    tree->Draw(var+s, "abs(Ditrk_mass-1.019)<0.075 && abs(Dimu_mass-3.1)<0.1");
+    TH1F *h1 = (TH1F*)gDirectory->Get("h1");
+      
+    RooRealVar x(var, var, down, up);
+    x.setBins(100);
+    
+    RooDataHist data("data", h1->GetTitle(), RooArgSet(x), Import(*h1, kFALSE));
+    x.setRange("R1", 4.9, 5.1);
+    x.setRange("R2", 5.1, 5.4);
+    x.setRange("R3", 5.4, 5.6);
+    
+    // Creare il fondo
+    RooRealVar c1("c1", "c1", -0.2, -10, 10);
+    RooRealVar c2("c2", "c2", -0.2, -10, 10);
+    RooRealVar c3("c3", "c3", -0.2, -10, 10);
+    
+    RooExponential pol_bkg("pol_bkg", "pol_bkg", x, c1);
+    pol_bkg.fitTo(data,Range("R1,R3"));
+    
+    // Creare la gaussiana
+    RooRealVar mean("mean", "Media gaussiana", (up+down)/2, down, up);
+    RooRealVar sigma("sigma", "Deviazione standard gaussiana", 0.02, 0.001, 0.3);
+    RooGaussian voigt_pdf("voigt_pdf", "Signal Gaussian PDF", x, mean, sigma);
+
+    // Creare la gaussiana
+    RooRealVar mean2("mean2", "Media gaussiana2", (up+down)/2, down, up);
+    RooRealVar sigma2("sigma2", "Deviazione standard gaussiana2", 0.05, 0.001, 0.3);
+    RooGaussian voigt_pdf2("voigt_pdf2", "Signal Gaussian PDF2", x, mean, sigma2);
+    
+    // Creare il modello di fit combinando fondo e gaussiana
+    RooRealVar nsig("nsig", "Numero di segnali", 0.25e+07, 1e+05, 1e+07);
+    RooRealVar nsig2("nsig2", "Numero di segnali2", 0.25e+07, 1e+05, 1e+07);
+    RooRealVar nbkg("nbkg", "Numero di background",0.5e+07, 1e+05, 1e+07);
+
+    RooAddPdf model("model", "Signal + Background", RooArgList(voigt_pdf, voigt_pdf2, pol_bkg), RooArgList(nsig, nsig2, nbkg));
+
+    RooFitResult *result = model.fitTo(data, Save(true));
+    
+    RooPlot *frame = x.frame();
+    data.plotOn(frame);
+    model.plotOn(frame, Components(voigt_pdf, voigt_pdf2), LineStyle(kDashed), LineColor(kRed));
+    model.paramOn(frame, Parameters(RooArgSet(nsig, nsig2, nbkg, mean, sigma)), Layout(0.1,0.4,0.9));
     model.plotOn(frame, Components(pol_bkg), LineStyle(kDashed), LineColor(kGreen));
     model.plotOn(frame);
     
