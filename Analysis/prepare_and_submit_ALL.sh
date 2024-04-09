1b151be8b94d7496ef2be41a97656c3a485b6c7c
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-04-09 15:43:57 +0200

    Update prepare_and_submit_ALL.sh

--- Analysis/prepare_and_submit_ALL.sh
+++ Analysis/prepare_and_submit_ALL.sh
@@ -16,7 +16,7 @@ if [ -z ${3+x} ]; then
 fi
 
 declare -a Era_2022=("C" "D-v1" "D-v2" "E" "F" "G")
-declare -a Era_2023=("C-v1" "C-v2" "C-v3" "C-v4" "D-v1" "D-v2")
+declare -a Era_2023=("B" "C-v1" "C-v2" "C-v3" "C-v4" "D-v1" "D-v2")
 
 if [ "${year}" == "2022" ]; then
   eras=("${Era_2022[@]}")
