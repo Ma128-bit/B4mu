0a183b050d94dd581b43bcde06b1d8473436e517
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-05-03 17:39:30 +0200

    Update B0Bkg.py

--- Utilities/B0Bkg.py
+++ Utilities/B0Bkg.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     treename = "FinalTree"
     #file = "../Analysis/FinalFiles_B2mu2K/Analyzed_Data_B2mu2K_2022.root"
     #file = "../Analysis/FinalFiles_B2mu2K/Analyzed_MC_B2mu2K_2022.root"
-    file = "../Analysis/FinalFiles_B2muKpi/Analyzed_MC_2K_with_B2muKpi_2022.root"
+    file = "../Analysis/FinalFiles_B2mu2K/Analyzed_MC_Kpi_with_B2mu2K_2022.root"
     rdf = RDataFrame(treename, file)
     print("Load RDF Done!")
     rdf = rdf.Define("B0Kpi","B0KpiMass(Mu1_Pt, Mu1_Eta, Mu1_Phi, Mu2_Pt, Mu2_Eta, Mu2_Phi, Mu3_Pt, Mu3_Eta, Mu3_Phi, Mu4_Pt, Mu4_Eta, Mu4_Phi)") 
