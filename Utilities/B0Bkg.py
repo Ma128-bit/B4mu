525d87fd796d2789d614c8ac99b299e3f40805a2
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-05-03 17:25:50 +0200

    Update B0Bkg.py

--- Utilities/B0Bkg.py
+++ Utilities/B0Bkg.py
@@ -96,7 +96,7 @@ if __name__ == "__main__":
     bkg_Bs = RooExponential("bkg_Bs", "bkg_Bs", x, c1)
 
     nsig = RooRealVar("nsig", "Numero di segnali", 100000, 50000, 1000000)
-    nsig2 = RooRealVar("nsig2", "nsig2", 1, 1000000)
+    nsig2 = RooRealVar("nsig2", "nsig2", 1, 100000)
     nbkg = RooRealVar("nbkg", "Numero di background",70000, 10000, 100000)
     model = RooAddPdf("model", "Signal + Background", RooArgList(signal_Bs, signal_Bd, bkg_Bs), RooArgList(nsig, nsig2, nbkg))
     
