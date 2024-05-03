32e601d0a29e26b8a621d93b18a9f256a61db882
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-05-03 15:57:28 +0200

    Update B0Bkg.py

--- Utilities/B0Bkg.py
+++ Utilities/B0Bkg.py
@@ -46,8 +46,8 @@ from ROOT import flat0D_double
 
 if __name__ == "__main__":
     treename = "FinalTree"
-    #file = "../Analysis/FinalFiles_B2mu2K/Analyzed_Data_B2mu2K_2022.root"
-    file = "../Analysis/FinalFiles_B2muKpi/Analyzed_MC_B2muKpi_2022.root"
+    file = "../Analysis/FinalFiles_B2mu2K/Analyzed_Data_B2mu2K_2022.root"
+    #file = "../Analysis/FinalFiles_B2muKpi/Analyzed_MC_B2muKpi_2022.root"
     rdf = RDataFrame(treename, file)
     print("Load RDF Done!")
     rdf = rdf.Define("B0Kpi","B0KpiMass(Mu1_Pt, Mu1_Eta, Mu1_Phi, Mu2_Pt, Mu2_Eta, Mu2_Phi, Mu3_Pt, Mu3_Eta, Mu3_Phi, Mu4_Pt, Mu4_Eta, Mu4_Phi)") 
