67464ef11a8fc959263d0f12291f2f1ca2806203
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-05-13 11:02:15 +0200

    Update Analizer.py

--- Analysis/Analizer.py
+++ Analysis/Analizer.py
@@ -208,8 +208,8 @@ if __name__ == "__main__":
         df = df.Define("Quadruplet_indexs","B2muX_QuadSel(remove_duplicate, isMC, evt, MuonPt, MuonEta, MuonPhi, RefTrack1_Pt, Mu1_Pt, Mu2_Pt, Mu3_Pt, Mu4_Pt, Mu3_Eta, Mu4_Eta, NGoodQuadruplets, QuadrupletVtx_Chi2, Quadruplet_Mass, Muon_isGlobal, Muon_isPF, Muon_isLoose, Muon_isMedium, Muon_isTight, Muon_isSoft, MuonPt_HLT, MuonEta_HLT, MuonPhi_HLT, FlightDistBS_SV_Significance, Muon_vz, GenParticle_Pt, GenParticle_Pt_v2, GenParticle_Eta_v2, GenParticle_Phi_v2, GenParticle_PdgId, GenParticle_MotherPdgId, GenParticle_GrandMotherPdgId, vtx_prob, QuadrupletVtx_x, QuadrupletVtx_y, RefittedPV_x, RefittedPV_y, Quadruplet_Pt, Quadruplet_Eta, Quadruplet_Phi)")
     df = df.Filter("Quadruplet_indexs[0]>-1")
 
-    #for chi in range(1):
-    for chi in range(5):
+    #for chi in range(5):
+    for chi in range(1):
         start_2 = time.time()
         branches=["evt", "isMC", "run", "lumi"]
         rdf = df.Define("Quadruplet_index", flat0D_int(chi), ["Quadruplet_indexs"])
