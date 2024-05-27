f1472a9db363b4c4a6151ac8bc34e27eec9abf09
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-05-27 10:46:42 +0200

    Update Analizer.py

--- Analysis/Analizer.py
+++ Analysis/Analizer.py
@@ -67,7 +67,7 @@ def QuadMuVar(rdf, branches, analysis_type):
             quadruplet_related_var.append("Vtx"+str(i)+str(j)+"_Chi2")
             quadruplet_related_var.append("Vtx"+str(i)+str(j)+"_nDOF")
             quadruplet_related_var.append("Vtx"+str(i)+str(j)+"_mass")
-            #quadruplet_related_var.append("Vtx"+str(i)+str(j)+"_mass_err")
+            quadruplet_related_var.append("Vtx"+str(i)+str(j)+"_mass_err")
     
     for var in quadruplet_related_var:
         branches.append(var)
