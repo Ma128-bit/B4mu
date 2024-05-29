8399d62291cf750386bb604f3da980feff517bcf
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-05-29 12:56:11 +0200

    Update Analizer.py

--- Analysis/Analizer.py
+++ Analysis/Analizer.py
@@ -236,7 +236,7 @@ if __name__ == "__main__":
     #for chi in range(5):
     for chi in range(1):
         start_2 = time.time()
-        branches=["evt", "isMC", "run", "lumi"]
+        branches=["evt", "isMC", "run", "lumi", "nPileUpInt"]
         rdf = df.Define("Quadruplet_index", flat0D_int(chi), ["Quadruplet_indexs"])
         rdf = rdf.Filter("Quadruplet_index>-1")
         branches.append("chi2_label")
