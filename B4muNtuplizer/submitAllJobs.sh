ec8460da98efa081105bcd5a5049b54a1c3be92c
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-07-31 17:21:34 +0200

    Update submitAllJobs.sh

--- B4muNtuplizer/submitAllJobs.sh
+++ B4muNtuplizer/submitAllJobs.sh
@@ -6,34 +6,47 @@ helpstring="Usage:
 submitAllJobs.sh [Year]"
 
 year=$1
+MCflag=$2
 
-if [ -z ${1+x} ]; then
+if [ -z ${2+x} ]; then
     echo -e ${helpstring}
+    return
+fi
+
+MCflag=$(echo "$MCflag" | tr '[:upper:]' '[:lower:]')
+
+if [ "$MCflag" != "true" ] && [ "$MCflag" != "false" ]; then
+    echo "$helpstring"
     return
 fi
 
 declare -a era2022preE=(("C" "D-v1" "D-v2" "E")
 declare -a era2022postE=("F" "G")
 declare -a era2023=("B" "C-v1" "C-v2" "C-v3" "C-v4" "D-v1" "D-v2")
+declare -a MCeras=("MC_pre" "MC_post")
 
 current_dir=$(pwd)
 
-if [[ "$current_dir" == *"CMSSW_13_"* ]]; then
-    if [[ "$year" == "2022" ]]; then
-        era=("${era2022postE[@]}")
-    elif [[ "$year" == "2023" ]]; then
-        era=("${era2023[@]}")
-    else
-        return
-    fi
-elif [[ "$current_dir" == *"CMSSW_12_"* ]]; then
-    if [[ "$year" == "2022" ]]; then
-        era=("${era2022preE[@]}")
+if [ "$MCflag" == "false" ]; then
+    if [[ "$current_dir" == *"CMSSW_13_"* ]]; then
+        if [[ "$year" == "2022" ]]; then
+            era=("${era2022postE[@]}")
+        elif [[ "$year" == "2023" ]]; then
+            era=("${era2023[@]}")
+        else
+            return
+        fi
+    elif [[ "$current_dir" == *"CMSSW_12_"* ]]; then
+        if [[ "$year" == "2022" ]]; then
+            era=("${era2022preE[@]}")
+        else
+            return
+        fi
     else
         return
     fi
 else
-    return
+    era=("${MCeras[@]}")
 fi
 
 cd CrabSubmission
