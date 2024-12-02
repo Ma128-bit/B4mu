6c43f5d4fc531fb35552a1dce2bc8e9749193a53
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-12-02 14:27:05 +0100

    Update Utilities.h

--- Analysis/Utilities.h
+++ Analysis/Utilities.h
@@ -491,8 +491,8 @@ vector<int> B2muX_QuadSel(vector<int> indexPreSel, int isMC, int evt, ROOT::VecO
         if(exit_code<1) exit_code=1;
         
         //Cut2 FlightDistBS_SV_Significance, dR and dz
-        if(FlightDistBS_SV_Significance.at(j) < 4 ) continue;
-        if(!(Cos2D_(QuadrupletVtx_x.at(j), QuadrupletVtx_y.at(j), RefittedPV_x.at(j), RefittedPV_y.at(j), Quadruplet_Pt.at(j), Quadruplet_Eta.at(j), Quadruplet_Phi.at(j))>0.95)) { continue;}
+        //if(FlightDistBS_SV_Significance.at(j) < 4 ) continue;
+        //if(!(Cos2D_(QuadrupletVtx_x.at(j), QuadrupletVtx_y.at(j), RefittedPV_x.at(j), RefittedPV_y.at(j), Quadruplet_Pt.at(j), Quadruplet_Eta.at(j), Quadruplet_Phi.at(j))>0.95)) { continue;}
         
         //Cut2 CMS muon system acceptance
         bool acceptanceCUT = true;
@@ -528,8 +528,8 @@ vector<int> B2muX_QuadSel(vector<int> indexPreSel, int isMC, int evt, ROOT::VecO
             isLoose = isLoose + Muon_isLoose.at(index.at(k));
             isPF = isPF + Muon_isPF.at(index.at(k));
         }
-        if(!(isLoose==4)) continue;
-        //if(!(isMedium==2 && isGlobal==2)) continue;
+        //if(!(isLoose==2)) continue;
+        if(!(isMedium==2 && isGlobal==2)) continue;
         if(exit_code<4) exit_code=4;
         
         //Cut5 HLT Trigger Matching
