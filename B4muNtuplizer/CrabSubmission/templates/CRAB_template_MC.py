6b50c8988e85cb3b542ddee71b44253bb171e5f6
Marco <127106076+Ma128-bit@users.noreply.github.com>
2026-02-04 13:31:08 +0100

    Remove ignoreGlobalBlacklist setting from config

--- B4muNtuplizer/CrabSubmission/templates/CRAB_template_MC.py
+++ B4muNtuplizer/CrabSubmission/templates/CRAB_template_MC.py
@@ -20,4 +20,3 @@ config.Data.unitsPerJob = 300
 config.Data.outputDatasetTag = 'SkimB4Mu_YEAR_ERANAME_B_TYPE_Mini'
 config.JobType.allowUndistributedCMSSW = True 
 config.Site.storageSite = 'T2_IT_Bari'
-config.Site.ignoreGlobalBlacklist  = True
